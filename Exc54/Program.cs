b0d18406f3f158cb78c6b0fd5383687129d17bdc
Kostas <langas.kostya@mail.ru>
2022-08-18 18:00:49 +0500

    упорядочить по убыванию элементы каждой строки двумерного массива.

--- Exc54/Program.cs
+++ Exc54/Program.cs
@@ -1,67 +1,44 @@
 ﻿// // Задайте двумерный массив. 
 // // Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
 
+Console.WriteLine("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int c = Convert.ToInt32(Console.ReadLine());
 
- class Program
+int[,] matrix = new int[n, c];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < c; j++)
     {
-        static void Main(string[] args)
-        {
-            Console.Write("Введите число строк: ");
-            int x = int.Parse(Console.ReadLine());
-            Console.Write("Введите число столбцов: ");
-            int y = int.Parse(Console.ReadLine());
-            int[,] mas = new int[x, y];
-            Console.WriteLine();
-            initialization(mas);
-            Sort(mas);
-            Console.WriteLine();
-            Print(mas);
- 
-        }
-        static void Print(int[,] arr)
-        {
-           for (int i = 0; i < arr.GetLength(0); i++)
-            {
-                for (int j = 0; j < arr.GetLength(1); j++)
-                    Console.Write("{0}\t", arr[i, j]);
-                    Console.WriteLine();
-            }
-        }
- 
-        static void initialization(int[,] mas)
-        {
- 
-            Random rand = new Random();
-            for (int i = 0; i < mas.GetLength(0); i++)
-            {
-                for (int j = 0; j < mas.GetLength(1); j++)
-                {
-                    mas[i, j] = rand.Next(1, 15);
-                    Console.Write("{0}\t", mas[i, j]);
-                }
-                Console.WriteLine();
-            }
-        }
- 
-        public static void Sort(int[,] mas)
-        {
- 
-      int x, i,j,b;
-            
-            for (i = 0; i < mas.GetLength(0); i++)
-    {
- 
-                for (j = 0; j < mas.GetLength(1); j++)
-                {
-                    x = mas[i, j];
- 
-                    for (b = j - 1; b >= 0 && mas[i,b] > x; b--)
-                        mas[i, b + 1] = mas[i, b];
- 
-                    mas[i, b + 1] = x;
-                }
-            }
-    
-        }
-       
+    matrix[i, j] = new Random().Next(0, 10);
+    Console.Write(matrix[i, j] + " \t");
     }
+    Console.WriteLine();
+}
+
+for (int a = 0; a < matrix.GetLength(0);  a++)
+{
+    for (int i = 0; i < matrix.GetLength(0) ; i++) 
+    {    
+        for (int j = 0; j < matrix.GetLength(1) - 1; j++)
+        {
+            if (matrix[i,j]  > matrix[i, j+ 1] )
+            {
+                int temp = matrix[i,j];
+                matrix[i,j] = matrix[i,  j+ 1];
+                matrix[i, j+ 1] = temp;
+            }
+        }
+    }    
+}
+Console.WriteLine();
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+    Console.Write(matrix[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
