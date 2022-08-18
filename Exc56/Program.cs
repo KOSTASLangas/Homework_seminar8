19473cf7dc94cac88defc28caf7af5ba5f0f88e1
Kostas <langas.kostya@mail.ru>
2022-08-18 20:11:43 +0500

    находить строку с наименьшей суммой элементов

---
+++ Exc56/Program.cs
@@ -0,0 +1,34 @@
+﻿//Задайте прямоугольный двумерный массив. 
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+
+int n = 6;
+int c = 3;
+
+int[,] matrix = new int[n, c];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+    matrix[i, j] = new Random().Next(0, 10);
+    Console.Write(matrix[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
+int sum = int.MaxValue;
+int index = 0;
+for (int i = 0; i < n; i++)
+{
+    int temp = 0;
+    for (int j = 0; j < c; j++)
+    {
+        temp += matrix[i, j];
+    }
+    if (temp < sum)
+    {
+        sum = temp;
+        index = i +1;
+    }
+}
+Console.WriteLine();
+Console.WriteLine("Строка: "+index+" Сумма: "+sum);
