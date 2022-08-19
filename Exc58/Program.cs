f5cf3e1acb2e0a4de8d50e1f93566c7e85bdb06c
Kostas <langas.kostya@mail.ru>
2022-08-19 14:10:55 +0500

    произведение двух матриц

---
+++ Exc58/Program.cs
@@ -0,0 +1,46 @@
+﻿//  Задайте две матрицы. Напишите программу, которая будет находить произведение двух матриц.
+
+Console.WriteLine("Введите кол-во строк: ");
+int n = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите кол-во столбцов: ");
+int c = Convert.ToInt32(Console.ReadLine());
+
+int[,] matrix = new int[n, c];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+    matrix[i, j] = new Random().Next(0, 10);
+    Console.Write(matrix[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
+Console.Write("Первая матрица");
+Console.WriteLine();
+int[,] matrix2 = new int[n, c];
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+    matrix2[i, j] = new Random().Next(0, 10);
+    Console.Write(matrix2[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
+Console.Write("Вторая матрица");
+Console.WriteLine();
+
+int[,] matrix3 = new int[n, c];
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < c; j++)
+    {
+    matrix3[i, j] = matrix[i,j] * matrix2[i, j];
+    
+    Console.Write(matrix3[i, j] + " \t");
+    }
+    Console.WriteLine();
+}
+Console.Write("Матрица произведений");
